1d7dc3d16231adfdb605035bcfbb3ed71906868c
106hi <hiro3428shs96kzsi@gmail.com>
2023-08-06 08:18:10 +0000

    Add practice10.java to Practice folder

---
+++ Practice/practice10.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+        int x = 7;
+        System.out.println(x);
+        System.out.println(x * x);
+        System.out.println(x * x * x);
+    }
+}
