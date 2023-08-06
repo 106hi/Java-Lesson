19126fc9811dcab5221dd3793d30280fd6c5abbe
106hi <hiro3428shs96kzsi@gmail.com>
2023-08-06 07:12:55 +0000

    Add practice3.java to Practice folder

---
+++ Practice/practice3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args) {
+        int x;
+        int y;
+        x = 13;
+        y = 17;
+        System.out.println("x=" + x + ",y=" + y);
+    }
+}
