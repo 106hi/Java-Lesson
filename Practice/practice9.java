968b953e8e938fda3a5f6e1469e7b29d9f7ce4ca
106hi <hiro3428shs96kzsi@gmail.com>
2023-08-06 08:13:26 +0000

    Add practice9.java to Practice folder

---
+++ Practice/practice9.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+        int x = 38;
+        System.out.println(x * 2);
+        System.out.println(x * 3);
+        System.out.println(x * 4);
+    }
+}
