af9f2cf3e3f8d8c944f4e3963c59f8e8b931a263
106hi <hiro3428shs96kzsi@gmail.com>
2023-08-06 08:09:03 +0000

    Add practice8.java to Practice folder

---
+++ Practice/practice8.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args) {
+        int x, y, z;
+        x = 19;
+        y = 23;
+        z = x * y;
+        System.out.println("z=" + z);
+    }
+}
