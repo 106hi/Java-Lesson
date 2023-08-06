f0b6b27cc60e9f8e490e7c4ecf8d2ff208cdd7c0
106hi <hiro3428shs96kzsi@gmail.com>
2023-08-06 08:28:16 +0000

    Add practice12.java to Practice folder

---
+++ Practice/practice12.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args) {
+        int x = 19;
+
+        x++;
+        System.out.println(x);
+        x--;
+        x--;
+        System.out.println(x);
+    }
+}
